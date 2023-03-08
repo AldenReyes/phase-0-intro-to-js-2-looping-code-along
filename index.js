c16db37783e690c698fe4983f0c8abe5051ac194
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-07 20:32:52 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const eventName = 'surprise';
+const message = [];
+
+function writeCards(names, eventName) {
+  for (let i = 0; i < names.length; i += 1) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+return(message);
+}
+
+function countDown(n) {
+  for (let count = n; count >= 0; count -=1) {
+    console.log(count);
+  }
+}
+
